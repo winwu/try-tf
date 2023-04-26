38c53132ef2033ec09725c5e6e89f1739f6b3d75
winwu <look_me08@hotmail.com>
2023-04-25 22:21:59 -0700

    update alb main.tf

--- try-aws-alb/main.tf
+++ try-aws-alb/main.tf
@@ -25,7 +25,6 @@ module "vpc" {
   }
 }
 
-
 module "web_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
@@ -34,4 +33,41 @@ module "web_sg" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["10.0.0.0/16"]
+}
+
+module "lb_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "lb-sg"
+  description = "security group for load balancer with HTTP ports"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/pet
+resource "random_pet" "app" {
+  length    = 2
+  separator = "-"
+}
+
+resource "aws_lb" "app" {
+  name               = "app-${random_pet.app.id}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [module.vpc.public_subnets]
+  security_groups    = [module.lb_sg.security_groups_id]
+}
+
+resource "aws_lb_listener" "app" {
+  # direct all traffic to blue load balancing target group on port 80
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  # route traffic to the target group
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.blue.arn
+  }
 }
