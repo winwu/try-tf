cf46230960f64bd8761198ecbf22675b8c36cd76
winwu <look_me08@hotmail.com>
2023-04-28 21:35:13 -0700

    try-aws-alb WIP

---
+++ try-aws-alb/blue.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "blue" {
+  # number of instance in blue
+  count                  = var.enable_blue_env ? 2 : 0
+  ami                    = "ami-014d05e6b24240371"
+  instance_type          = "t2.micro"
+  subnet_id              = module.vpc.private_subnets[count.index % length(module.vpc.private_subnets)]
+  vpc_security_group_ids = [module.web_sg.this_security_group_id]
+  user_data = templatefile("${path.module}/init-script.sh", {
+    file_content = "Version 1.0 - #${count.index}"
+  })
+
+
+  tags = {
+    Name = "blue of ${count.index}"
+  }
+}
